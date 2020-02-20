29e5e2fced09a72aa9ce8babc2811a566d745196
Javi <franfdk17@gmail.com>
2020-02-20 13:37:25 +0100

    reduce_mem_usage fix

--- murcIA.py
+++ murcIA.py
@@ -85,35 +85,42 @@ print("  plot_cat(pandas_series)")
 
 ##################################################################### LOADING DATA
 
-
+# int8    Integer (-128 to 127)
+# int16   Integer (-32768 to 32767)
+# int32   Integer (-2147483648 to 2147483647)
+# int64   Integer (-9223372036854775808 to 9223372036854775807)
+# uint8   Unsigned integer (0 to 255)
+# uint16  Unsigned integer (0 to 65535)
+# uint32  Unsigned integer (0 to 4294967295)
+# uint64  Unsigned integer (0 to 18446744073709551615)
 def reduce_mem_usage(df):
     start_mem = df.memory_usage().sum() / 1024**2
-    print('Memory usage of dataframe is {:.3f} MB'.format(start_mem))
 
     for col in df.columns:
         col_type = df[col].dtype
-    if col_type != object:
-        c_min = df[col].min()
-        c_max = df[col].max()
-        if str(col_type)[:3] == 'int':
-            if   c_min > np.iinfo(np.int8).min    and c_max < np.iinfo(np.int8).max:   df[col] = df[col].astype(np.int8)
-            elif c_min > np.iinfo(np.uint8).min   and c_max < np.iinfo(np.uint8).max:  df[col] = df[col].astype(np.uint8)
-            elif c_min > np.iinfo(np.int16).min   and c_max < np.iinfo(np.int16).max:  df[col] = df[col].astype(np.int16)
-            elif c_min > np.iinfo(np.uint16).min  and c_max < np.iinfo(np.uint16).max: df[col] = df[col].astype(np.uint16)
-            elif c_min > np.iinfo(np.int32).min   and c_max < np.iinfo(np.int32).max:  df[col] = df[col].astype(np.int32)
-            elif c_min > np.iinfo(np.uint32).min  and c_max < np.iinfo(np.uint32).max: df[col] = df[col].astype(np.uint32)
-            elif c_min > np.iinfo(np.int64).min   and c_max < np.iinfo(np.int64).max:  df[col] = df[col].astype(np.int64)
-            elif c_min > np.iinfo(np.uint64).min  and c_max < np.iinfo(np.uint64).max: df[col] = df[col].astype(np.uint64)
-        else:
-            if   c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max: df[col] = df[col].astype(np.float16)
-            elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max: df[col] = df[col].astype(np.float32)
-            elif c_min > np.finfo(np.float64).min and c_max < np.finfo(np.float64).max: df[col] = df[col].astype(np.float64)
-    else:
-        df[col] = df[col].astype('category')
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if   c_min >= np.iinfo(np.uint8).min   and c_max <= np.iinfo(np.uint8).max:  df[col] = df[col].astype(np.uint8)
+                elif c_min >= np.iinfo(np.int8).min    and c_max <= np.iinfo(np.int8).max:   df[col] = df[col].astype(np.int8)
+                elif c_min >= np.iinfo(np.uint16).min  and c_max <= np.iinfo(np.uint16).max: df[col] = df[col].astype(np.uint16)
+                elif c_min >= np.iinfo(np.int16).min   and c_max <= np.iinfo(np.int16).max:  df[col] = df[col].astype(np.int16)
+                elif c_min >= np.iinfo(np.uint32).min  and c_max <= np.iinfo(np.uint32).max: df[col] = df[col].astype(np.uint32)
+                elif c_min >= np.iinfo(np.int32).min   and c_max <= np.iinfo(np.int32).max:  df[col] = df[col].astype(np.int32)
+                elif c_min >= np.iinfo(np.uint64).min  and c_max <= np.iinfo(np.uint64).max: df[col] = df[col].astype(np.uint64)
+                elif c_min >= np.iinfo(np.int64).min   and c_max <= np.iinfo(np.int64).max:  df[col] = df[col].astype(np.int64)
+            else:
+                if   c_min >= np.finfo(np.float16).min and c_max <= np.finfo(np.float16).max: df[col] = df[col].astype(np.float16)
+                elif c_min >= np.finfo(np.float32).min and c_max <= np.finfo(np.float32).max: df[col] = df[col].astype(np.float32)
+                elif c_min >= np.finfo(np.float64).min and c_max <= np.finfo(np.float64).max: df[col] = df[col].astype(np.float64)
+        #else:
+        #    df[col] = df[col].astype('category')
 
     end_mem = df.memory_usage().sum() / 1024**2
-    print('Memory usage after optimization is: {:.3f} MB'.format(end_mem))
-    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    percent = 100 * (start_mem - end_mem) / start_mem
+    print("Memory decreased from {:.2f} MB to {:.2f} MB ({:.1f}%)".format(start_mem, end_mem, percent))
+    
     return df
 
 
